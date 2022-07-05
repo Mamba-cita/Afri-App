178c8539d252b4fd85ca292fef1b3c82f426de27
Mamba-Cita <edwnsjohn52@gmail.cm>
2022-07-05 15:45:32 +0300

    included seachbar on the sidenavbar

--- static/js/public/public.js
+++ static/js/public/public.js
@@ -1,15 +1,35 @@
-console.log ('Hello World');
+console.log("AFRI LIMITED");
 
+//open and close the sidebar
+
+let btn = document.querySelector("#btn");
+let searchIcon = document.querySelector("#searchIcon");
+let sidebar = document.querySelector(".sidebar");
+let sidebarBtn = document.querySelector(".bx-menu");
+
+//open and close the sidebar
+btn.onclick = function () {
+  sidebar.classList.toggle("close");
+};
+sidebarBtn.onclick = function () {
+  sidebar.classList.toggle("close");
+};
+//console.log(sidebarBtn);
+//open and close the search bar
+searchIcon.onclick = function () {
+  sidebar.classList.toggle("close");
+};
+//console.log(searchIcon);
+
+// end of open and close the sidebar
+
+//selecting main parent of arrow/menu and displaying the submenu
 let arrow = document.querySelectorAll(".arrow");
-  for (var i = 0; i < arrow.length; i++) {
-    arrow[i].addEventListener("click", (e)=>{
-   let arrowParent = e.target.parentElement.parentElement;//selecting main parent of arrow
-   arrowParent.classList.toggle("showMenu");
-    });
-  }
-  let sidebar = document.querySelector(".sidebar");
-  let sidebarBtn = document.querySelector(".bx-menu");
-  console.log(sidebarBtn);
-  sidebarBtn.addEventListener("click", ()=>{
-    sidebar.classList.toggle("close");
+for (var i = 0; i < arrow.length; i++) {
+  arrow[i].addEventListener("click", (e) => {
+    let arrowParent = e.target.parentElement.parentElement;
+    arrowParent.classList.toggle("showMenu");
   });
+}
+
+//end of selecting main parent of arrow/menu and displaying the submenu
